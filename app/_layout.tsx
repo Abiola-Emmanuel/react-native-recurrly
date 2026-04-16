d26d2698103a7230b1e8b0c2407bb342130d039c
Abiola Emmanuel <sijuwolaabiola@gmail.com>
2026-04-16 19:38:55 +0100

    posthog integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/expo';
 import { tokenCache } from '@clerk/expo/token-cache';
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
+import { PostHogProvider } from 'posthog-react-native';
 import { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
@@ -36,8 +37,13 @@ export default function RootLayout() {
   if (!fontsLoaded) return null;
 
   return (
-    <ClerkProvider publishableKey={getPublishableKey()} tokenCache={tokenCache}>
-      <Stack screenOptions={{ headerShown: false }} />
-    </ClerkProvider>
+    <PostHogProvider
+      apiKey={process.env.EXPO_PUBLIC_POSTHOG_API_KEY!}
+      options={{ host: process.env.EXPO_PUBLIC_POSTHOG_HOST }}
+    >
+      <ClerkProvider publishableKey={getPublishableKey()} tokenCache={tokenCache}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ClerkProvider>
+    </PostHogProvider>
   );
 }
